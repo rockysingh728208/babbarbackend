4fe9dca4d831bcb34177c86d59624948e42cec1c
rockysingh728208 <sagarkashyap9155@example.com>
2025-06-20 21:39:22 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,20 @@
+const express = require('express')
+const app = express()
+const port = 4000;
+
+app.get('/', (req, res) => {
+  res.send('Hello World!')
+})
+app.get("/twitter",(req,res)=>{
+res.send("sagar kashyap")
+})
+app.get("/login",(req,res)=>{
+res.send('<h1>please login chai aaur code</h1>')
+})
+app.get("/youtube",(req,res)=>{
+res.send('<h2>chai or code</h2>')
+})
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
